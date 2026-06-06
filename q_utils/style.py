29f6864d5e2e4623f3cf9208b912d0ff39037289
vigneshwari311 <vigneshwarik10@gmail.com>
2026-06-06 23:58:10 +0530

    Deutsch Josza algorithm with noise simulator

---
+++ q_utils/style.py
@@ -0,0 +1,10 @@
+dark_theme = {
+    'backgroundcolor': '#0D1117',
+    'textcolor': '#FFFFFF',
+    'linecolor': '#8B949E',
+    'creglinecolor': '#8B949E',
+    'gatefacecolor': '#1F6FEB',
+    'gatetextcolor': '#FFFFFF',
+    'barrierfacecolor': '#30363D',
+    'scolor': '#58A6FF',
+}
